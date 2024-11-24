58f3d98583f36de557d6b6b7a2a70ab2873724e1
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-24 23:54:28 +0100

    logical operators - interactive "work with us" check

--- main.cpp
+++ main.cpp
@@ -10,18 +10,47 @@ int main() {
     //&&
     //||
 
-    bool a = true;
-    bool b = false;
-    bool c = false;
+    cout << "Are you eligible to work with us? Let's find out! " << endl;
 
-    bool result = b && !a;
-    cout << boolalpha << result << endl;
+    cout << "Do you have a Bachelor's degree?" << endl;
+    bool degree;
+    string degree_answer;
+    cin >> degree_answer;
+     if (degree_answer == "yes")
+    {
+         degree = true;
+     }
+    else {
+        degree = false;
+    }
 
-    result = a || b && c;
-    cout << boolalpha << result << endl;
+    cout << "Do you have at least 2 years of work experience?" << endl;
 
-    result = (a || b) && c;
-    cout << boolalpha << result << endl;
+    bool work;
+    string work_answer;
+    cin >> work_answer;
+    if (work_answer == "yes") {
+        work = true;
+    }
+    else {
+        work = false;
+    }
+    bool citizen;
+    cout << "Do you have a US citizenship?" << endl;
+    string citizen_answer;
+    cin >> citizen_answer;
 
+    if (citizen_answer == "yes") {
+        citizen = true;
+    }
+    else {
+        citizen = false;
+    }
+
+    string result = "Sorry, you are not eligible.";
+    if ((degree || work )&& citizen) {
+        result = "Congratulations, you qualify!";
+    }
+    cout << result << endl;
     return 0;
 }
