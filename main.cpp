9cf2d0203454a65b994db460bdd33942d11759c6
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-24 22:36:28 +0100

    comparisons and logical operators
    comparisons and logical operators

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main() {
 
-    int x = 10;
-    int y = 3;
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    cout << "Enter your salary: ";
+    float salary;
+    cin >> salary;
 
-    double z = (double)x / y; // C-style cast, may create issues/
-    double z_butBetter = static_cast<double>(x) / y; //static cast is better and safer
-    cout << z << endl
-         << z_butBetter << endl;
+    bool isEligible = (age > 18 && age < 65) || (age >65 && salary >= 3000); // || is for 'or' && is for 'and'
+    cout <<boolalpha << "Are you forbidden from participating? : " << !isEligible << endl; // ! is for inverting bool
     return 0;
 }
