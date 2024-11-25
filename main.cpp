cb4e775b6aa69a017de2bfb0622dcc5bfab41006
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-25 01:31:06 +0100

    for loop -  printing multiple numbers

--- main.cpp
+++ main.cpp
@@ -5,31 +5,8 @@ using namespace std;
 
 int main() {
 
-    double number1 {};
-    double number2 {};
-
-    cout << "Enter two numbers please? " << endl
-         << "Number 1:";
-    cin >> number1;
-    cout << "Number 2:";
-    cin >> number2;
-    cout << "Enter an opperator";
-    char op{};
-    cin >> op;
-
-    double result{};
-
-    bool opValid = true;
-    switch (op) {
-        case '+': result = number1 + number2; break;
-        case '-': result = number1 - number2; break;
-        case '*': result = number1 * number2; break;
-        case '/': result = number1 / number2; break;
-        //case '%': result = static_cast<double>(number1) % number2; break; // suggested by Clion, but gives error on operator, skipping for now
-        default: cout << "Invalid operation" << endl; opValid = false;
+    for ( int i = 0; i < 10; i++) {
+        cout << i+1 << endl;
     }
-    if (opValid) cout << "Result: " << result << endl;
-
-
     return 0;
 }
