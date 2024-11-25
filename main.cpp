b772480970e5aa9c55595030934a5fcc3c24adcc
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-25 04:01:11 +0100

    do while loop- guess my number (in range 1-5)

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@ using namespace std;
 
 int main() {
     int myNum = 7;
+    int number{};
+    int counter = 0;
 
-    int guess{};
-    while (guess != myNum) {
-        cout << "Guess my number : " << endl;
-        cin >> guess;
-        if (guess != myNum) cout << "nope" << endl;
-    }
+    // do while executes the block after do before evaluation of the condition
+    // thus it will always be run at least once
+    do {
+        cout << "Enter a number:";
+        cin >> number;
+        counter++;
+        if (counter > 5 && !(number>=1 && number<=5) ) cout << "Hint: it's smaller than 6" << endl;
+    } while (number<1 || number>5);
     cout << "yay!" << endl;
     return 0;
 }
