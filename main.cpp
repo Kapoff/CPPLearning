6e8559879ece5c379a0a4bb950ca10f9b2eb63ca
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-25 01:41:20 +0100

    for loop range based -  printing multiple numbers

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 using namespace std;
 
 int main() {
-
-    for ( int i = 0; i < 10; i++) {
-        cout << i+1 << endl;
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for ( int i = 0; i < sizeof(numbers)/sizeof(int); i++) { // cluncky, long, baaaad
+        cout << numbers[i] << endl;
     }
+    for (int number:numbers) // short, neat, niiiice
+        cout << number << endl;
     return 0;
 }
