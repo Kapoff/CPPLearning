0fafde61e73d84d977ad5527eb0b924e6f283a62
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-27 01:52:41 +0100

    nested loops- * printing in triangle shape * ** *** **** *****

--- main.cpp
+++ main.cpp
@@ -4,13 +4,20 @@
 using namespace std;
 
 int main() {
-    int array_size[] = {4, 3};
+    int array_size = 1;
+    int line = 1;
     cout << "Enter the size of the array:";
-    cin >> array_size[0] >> array_size[1];
+    cin >> array_size;
 
-    for (int x = 1; x <=array_size[0]; x++) {
-        for (int y=1; y<=array_size[1]; y++) {
-            cout << "(" << x << "," << y << ")" << endl;
+    for (int y = 1; y <= array_size; y++) { //number of lines
+
+        for (int x = 1; x<=y; x++) {
+            cout << "*"; //print star as many starts as number of line we in
+        }
+
+        if (line==y) {
+            cout << endl;
+            line++;
         }
     }
     return 0;
