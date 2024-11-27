7501a496c075e091634f50bb0f10f066af9f5f30
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-27 03:59:02 +0100

    nested loops- printing a xmass tree

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
+#include <cmath>
 #include <iostream>
-
+#include <iomanip>
 
 using namespace std;
 
 int main() {
     int array_size = 1;
-    int line = 1;
-    cout << "Enter the size of the array:";
+    cout << "Enter the size of the tree:";
     cin >> array_size;
+    int middle_line = round(static_cast<double>(array_size*2)/2);
 
-    for (int y = 1; y <= array_size; y++) { //number of lines
+    for (int y = 0; y < array_size; y++) { //number of lines
 
-        for (int x = 1; x<=y; x++) {
-            cout << "*"; //print star as many starts as number of line we in
+        // always print the pyramid size*2 because we build start in both directions from center
+        for (int x = 1; x<=array_size*2; x++) {
+
+                if (x >= middle_line-y && x <= middle_line+y)
+                    //only print symbols if in middle of line
+                    if (y == 0)                                     cout << "*";
+                    else if (x == middle_line-y )                   cout << "/";
+                    else if (x == middle_line+y)                    cout << "\\";
+                    else if (y == array_size-1)                     cout << "_";
+                    else                                            cout << ".";
+                else cout << " ";
         }
-
-        if (line==y) {
-            cout << endl;
-            line++;
-        }
+        cout << endl; //finish line
     }
+
     return 0;
 }
