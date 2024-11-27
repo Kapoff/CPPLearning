e0ccbae96f34ab1b57564af8855cfd73cffb7a55
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-27 07:55:47 +0100

    separating greet into it's own .cpp/.h

--- main.cpp
+++ main.cpp
@@ -2,22 +2,14 @@
 #include <iostream>
 #include <iomanip>
 
+#include "utils/greet.hpp"
+
 using namespace std;
-
-//function declaration (celling compiler that it exists, without specifying what it does)
-void greet (string name);
-
-
 
 
 int main() {
 
-    greet ("john"); // will still run,
-
+    greet ("john");
+    cout << "Hello World!" << endl;
     return 0;
-}
-
-//function definition (tells the compiler what the function does)
-void greet (string name) {
-    cout <<"hello" << name << endl;
 }
---
+++ utils/greet.cpp
@@ -0,0 +1,11 @@
+//
+// Created by alexe on 2024-11-27.
+//
+#include <iostream>
+
+
+using namespace std;
+
+void greet (string name) {
+    cout <<"Hello " << name << endl;
+}
