b92b426f89cd189b761e4d879974659b14de8784
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-29 03:34:06 +0100

    arrays - multidimensional arrays

--- main.cpp
+++ main.cpp
@@ -1,39 +1,31 @@
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+#include <bits/ranges_algo.h>
 
 #include "utils/greet.hpp"
 
 using namespace std;
 
-void bubbleSortArray(auto array[], int arraySize) {
-    for (int pass = 0; pass < arraySize; pass++) {
-        for (int i = 0; i < arraySize - 1; i++) {
-            if (array[i] > array[i + 1]) {
-                swap(array[i], array[i + 1]);
-            }
-        }
-    }
-
-
-
-}
-
 
 int main() {
+    float matrix [3][3] = {215.54, 41.546,564.1,
+                         -45,359.3,0,
+                         1,1,1};
+    float array [2][3] = {
+        {1,2,3},
+        {4,5,6}};
+//can be done both ways apparently
+    cout << "Transform matrix be like: " << endl;
 
-    int values [] = {5, 6, 1,2,468,4,6,7};
-
-    cout << " OG array is : ";
-    for (int i = 0; i < size(values); ++i)
-        cout << values[i] << " ";
-
-
-    cout << endl;
-    cout << " sorted array is : ";
-    bubbleSortArray(values, size(values));
-    for (int i = 0; i < size(values); ++i)
-        cout << values[i] << " ";
+    for ( int x = 0; x < size(matrix); x++) {
+        for ( int y = 0; y < size(matrix[x]); y++) {
+            cout <<setw(7) <<  matrix[x][y] << " ";
+        }
+        cout << endl;
+    }
+    cout << "array size: " << size(matrix) << " by " <<size(matrix[1]) << endl;
 
     return 0;
 }
