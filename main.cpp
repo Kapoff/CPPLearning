a1aa2b312822eb592afdaac2610ae1385be2ec55
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-29 04:16:23 +0100

    pointers - exercise - swap variable function

--- main.cpp
+++ main.cpp
@@ -8,24 +8,19 @@
 
 using namespace std;
 
-void makeShitBigger (float* number) { //passing the thing as a pointer
-    *number *= 1.2; //making the stuff inside the var that the pointer is pointing to bigger
+void swapVars (float* var1, float* var2) {
+    float temp = *var1;
+    *var1 = *var2;
+    *var2 = temp;
 }
 
 int main() {
     float shit = 100;
-    makeShitBigger(&shit); // passing the var by ref so the pointer can work
-    cout <<shit << endl;
-    float* pointy = &shit;
-    makeShitBigger(pointy); // instead of passing by  var, we can also input a pointer as arg.
-    cout <<*pointy;
+    float lolz = 144;
 
-    // makeShitBigger(shit); // inputting just the var wont work, c++ gets mad
+    cout << "OG order: " << shit << " " << lolz << endl;
 
-    /*
-     * in this simple case, it is simpler and more modern to pass the arg by ref with &varName instead of a pointer
-     * the pointer method is outdated byt can still be encountered in legacy code
-     */
-
+    swapVars(&shit,&lolz);
+    cout << "Swapped order: " << shit << " " << lolz << endl;
     return 0;
 }
