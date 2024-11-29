36b8d4e6c08238ef580b93914b03084eda8a3426
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-29 01:16:38 +0100

    arrays - sizeof() vs size()

--- main.cpp
+++ main.cpp
@@ -5,22 +5,21 @@
 #include "utils/greet.hpp"
 
 using namespace std;
-using messaging::greet; // using a specific function instead of the whole namespace
-
-void printOddNumbers (int limit) {
-    for (int i = 0; i < limit; i++) {
-        if (i % 2 == 0) // deliberate == instead of !=, to break odd number behaviour
-            cout << i << endl;
-    }
-}
 
 int main() {
+    int numbers[] = {19, 2, 34, 4, 5 };
+    cout <<"Arrays has " << size(numbers) << " numbers in it." << endl;
+
+    cout << "output with size()" << endl;
+    for ( int i = 0; i < size(numbers); i++) { // size() gives the size ofthe array directly
+        cout << numbers[i] << endl;
+    }
+
+    cout << "output with sizeof()" << endl;
+    for ( int i = 0; i < sizeof(numbers)/sizeof(int); i++) { // sizeof() relies of byt size of the element sampled.
+        cout << numbers[i] << endl;
+        }
 
 
-    greet ("john");
-    printOddNumbers (10);
-    // set breakpoint at fuction execution, because this is the offending part of the code that we want to diagnose
-        //then one in debuging, step into the line (F7) to get inside the funtion execution, and step over to get to the logic part
-        // you can also add custom code bits to watchers by copypasta in the debug panel, to track particular bits of code without variables
     return 0;
 }
