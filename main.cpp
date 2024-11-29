9adb998a6a051549d57acff260f3a2cf1e731356
ADMIT <alexei.dmitriev.k@gmail.com>
2024-11-29 01:59:47 +0100

    arrays - unpacking or structured binding

--- main.cpp
+++ main.cpp
@@ -6,23 +6,17 @@
 
 using namespace std;
 
-/*
-void printingNumbers(int numbers[]) {
-            // will not work, because numbers[] as passed though the functions args, is just a pointer
-    for (int i = 0; i < size(numbers); i++)
-        cout << numbers[i] << " ";
-}*/
-void printingNumbers(int numbers[], int arraySize) {
-    // need to add param to pass array size, in order to loop over array
-    for (int i = 0; i < arraySize; i++)
-        cout << numbers[i] << " ";
-}
+
 
 int main() {
-    int first[] = {1, 2, 3, 4, 5};
-    int other[] = {1, 2, 3, 4, 5, 434};
 
-    printingNumbers(first, size(first));
+    int values [3] = {1, 2, 3};
+    // int x = values[0];
+    // int y = values[1];
+    // int z = values[2];
 
+    //now the same thing byt better
+    auto [x,y,z] = values; //called unpacking in python and in C++: structured binding
+    cout << x << ", " << y << ", " << z << endl;
     return 0;
 }
